281b0a7d4c73d6754fc152e032436fee9b7ef309
Tim <tim@devze.ro>
2023-03-01 04:33:47 +0000

    support commander

--- src/lambdaFunc.js
+++ src/lambdaFunc.js
@@ -1,7 +1,9 @@
-function name(params) {
+const myfunc = function myfunc(params) {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello froma live demo!, not lambda!'),
+    body: JSON.stringify('Hello froma live demo!, not lambda! Now we\'re parsing node_modules!'),
   };
   return response;
-}
+};
+
+module.exports = myfunc
